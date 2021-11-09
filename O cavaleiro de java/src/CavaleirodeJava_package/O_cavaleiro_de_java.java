6c14e094d010e8caca11694a3db5938f5fd4b2d5
SamuelColina <paradoxo456@gmail.com>
2021-11-09 16:57:27 -0300

    Update O_cavaleiro_de_java.java

--- O cavaleiro de java/src/CavaleirodeJava_package/O_cavaleiro_de_java.java
+++ O cavaleiro de java/src/CavaleirodeJava_package/O_cavaleiro_de_java.java
@@ -1,6 +1,8 @@
 package CavaleirodeJava_package;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class O_cavaleiro_de_java {
 	//Para rodar o jogo sem o delay nas mensagens, pode mudar o valor destas 3 variáveis para 0;
@@ -62,16 +64,180 @@ public class O_cavaleiro_de_java {
 	    
 	    //esse trecho a seguir é apenas um protótipo de como vai funcionar o jogo, ainda não foi adicionado nenhum desafio nele
 		Delay("Você acorda com um homem muito velho e barbudo te levantando e gritando\n"
-				+ "Velho Barbudo: Não há tempo para explicar, você precisa salvar o mundo", TimeUnit.MILLISECONDS, temp_dialog);
-		System.out.print("\n 1- Quem é você? \n 2- Oque esta acontecendo?! \n 3- Tabom :)");
+				+ "Velho Barbudo: Não há tempo para explicar, você precisa fechar essa fissura", TimeUnit.MILLISECONDS, temp_dialog);
+		do {
+		System.out.print("\n 1- Quem é você? \n 2- Oque esta acontecendo?! \n 3- Eu não sei como fechala \n 4- Irei fechala");
 		seleciona = entrada.nextInt();
-		if(seleciona == 3) {
-			Delay("Velho barbudo: Estou surpreso de você não me pergutar nada... \n enfim, só você pode usar sua magia e resolver esse "
-					+ "enigma para fechar essa fissura", TimeUnit.MILLISECONDS, temp_dialog);
+		if(seleciona == 1) {
+			Delay("Velho barbudo: Como eu disse, não a tempo para explicar!, feche essa fissura", TimeUnit.MILLISECONDS, temp_dialog);
 		}
+		
+		if(seleciona == 2) {
+			Delay("Velho barbudo: Se esta fissura não for fechada, nós iremos morrer aqui, e só você pode fechala\n então se apresse", TimeUnit.MILLISECONDS, temp_dialog);
+		}
+		
+		if(seleciona == 3) {
+			Delay("Velho barbudo: Concentre-se apenas na lógica, pois você já possui a força", TimeUnit.MILLISECONDS, temp_dialog);
+		}
+		
+		
+		}while(seleciona != 4);
 		//Aqui é aonde se inicia com o primeiro desafio
+		df_SCrpg();
+		
+		Delay("A fissura se fecha, mas é possivel se escutar uma voz\n ??? : Maldito seja!!, os magos iram conhecer toda agonia deste mundo", TimeUnit.MILLISECONDS, temp_dialog);
+	}
+	
+	public static void df_ShuffleRpg() {
+		Scanner entrada = new Scanner(System.in);
+		String alt;
+		boolean sair = true;
+		
+		
+			System.out.printf("	Existe no java uma função para embaralhar conteúdos e coleções. Qual o nome da expressão?\n ");
+		
+			ArrayList<String> lista = new ArrayList<String>();
+				lista.add("Math.Random;");  // resposta correta
+				lista.add("Shuffle;");
+				lista.add("Sort;");
+				lista.add("Reverse;");
+				lista.add("Array;");
+			  //valores do array  
+
+		do{
+			Collections.shuffle(lista);      //sorteia as alternativas
+			for(int i = 0; i < lista.size(); i++){ //numera o array
+				switch (i){
+					case 0:
+						System.out.println("a) " + lista.get(i));
+						break;
+					case 1:
+						System.out.println("b) " + lista.get(i));
+						break;
+					case 2:
+						System.out.println("c) " + lista.get(i));
+						break;
+					case 3:
+						System.out.println("d) " + lista.get(i));
+						break;
+					case 4:
+						System.out.println("e) " + lista.get(i));
+			}
+		}
+			System.out.printf("\n Digite aqui a resposta correta: ");
+				alt = entrada.next();
+				int n = 0;
+				//pega a resposta e compara com o numero da lista
+			switch (alt){
+			case "a":
+			case "A":
+				n = 0;
+				break;
+			case "b":
+			case "B":
+				n = 0;
+				break;
+			case "c":
+			case "C":
+				n = 0;
+				break;
+			case "d":
+			case "D":
+				n = 0;
+				break;
+			case "e":
+			case "E":
+				n = 0;
+				break;
+			default:
+				System.out.println("Escolha inválida!");
+		}
+			if(lista.get(n).equals("Math.Random;")){ //compara a resposta com a alternativa
+				System.out.println("Resposta correta! :D");
+				sair = false;
+			}else {
+				System.out.println("Resposta incorreta! D:");
+			}
+			
+		} while(sair);
 		
 	}
+	
+	public static void df_SCrpg() {
+		Scanner entrada = new Scanner(System.in);
+		String alt;
+		boolean sair = true;
+		
+		
+			System.out.printf("\nQuais expressões no switch são opcionais? \n ");
+		
+			ArrayList<String> lista = new ArrayList<String>();
+				lista.add("Break e Default;");  // resposta correta
+				lista.add("Case e Break;");
+				lista.add("Else e Case;");
+				lista.add("Default e While;");
+				lista.add("While e Else;");
+			  //valores do array  
+
+		do{
+			Collections.shuffle(lista);      //sorteia as alternativas
+			for(int i = 0; i < lista.size(); i++){ //numera o array
+				switch (i){
+					case 0:
+						System.out.println("a) " + lista.get(i));
+						break;
+					case 1:
+						System.out.println("b) " + lista.get(i));
+						break;
+					case 2:
+						System.out.println("c) " + lista.get(i));
+						break;
+					case 3:
+						System.out.println("d) " + lista.get(i));
+						break;
+					case 4:
+						System.out.println("e) " + lista.get(i));
+			}
+		}
+			System.out.printf("\n Digite aqui a resposta correta: ");
+				alt = entrada.next();
+				int n = 0;
+				//pega a resposta e compara com o numero da lista
+			switch (alt){
+			case "a":
+			case "A":
+				n = 0;
+				break;
+			case "b":
+			case "B":
+				n = 0;
+				break;
+			case "c":
+			case "C":
+				n = 0;
+				break;
+			case "d":
+			case "D":
+				n = 0;
+				break;
+			case "e":
+			case "E":
+				n = 0;
+				break;
+			default:
+				System.out.println("Escolha inválida!");
+		}
+			if(lista.get(n).equals("Break e Default;")){ //compara a resposta com a alternativa
+				System.out.println("Resposta correta! :D");
+				sair = false;
+			}else {
+				System.out.println("Resposta incorreta! D:");
+			}
+			
+		} while(sair);
+		
+	}
+	
 	
 	//Este método é o responsavel para dar um "delay" nas messagens do jogo
 	public static void Delay(String mensagem, TimeUnit unit, long tempo_mensagem) throws InterruptedException {
